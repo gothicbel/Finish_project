39bd5c86bb5b19cbadbb3f5b3162b158163312bc
gothicbel <gothicbel@yandex.ru>
2025-08-07 14:51:37 +0100

    Теперь автотест проходит все шаги

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -9,12 +9,5 @@ def post_new_order(body):
                          headers=data.headers)
 
 
-response = post_new_order(data.create_order_body)
-track = response.json()["track"]
-
-
-def get_track_order():
-    return requests.get(configuration.URL_SERVICE + configuration.TRACK_ORDER + "?t=" + str(track))
-
-
-response = get_track_order()
+def get_track_order(track):
+    return requests.get(configuration.URL_SERVICE + configuration.TRACK_ORDER + track)
